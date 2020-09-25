31b6049b1b52bc829aaa976312365f426576e29c
Lola <43297230+aliciacactus@users.noreply.github.com>
2020-09-25 23:23:01 +0200

    add(Events): Added guildCreate event | add(Command): added eval command

---
+++ commands/owner/eval.js
@@ -0,0 +1,53 @@
+const util = require('util');
+
+const Command = require('../../structures/Command');
+const config = require('../../config.json')
+
+module.exports = class EvalCommand extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'eval',
+            usage: 'eval <code>',
+            description: 'Executing code.',
+            type: 'owner'
+        })
+    }
+
+    async run(message, args) {
+        try {
+            const returned = eval(args.join(" "));
+            let str = util.inspect(returned, {
+                depth: 1
+            })
+            if (str.length > 1900) {
+                str = `${str.substr(0, 1897)}...`
+            }
+            str.replace(new RegExp(config.token, 'gi'), '( ͡° ͜ʖ ͡°)')
+            message.channel.send('```xl\n' + str + '\n```').then(ms => {
+                if (returned != undefined && returned !== null && typeof returned.then === 'function') {
+                    returned.then(() => {
+                        str = util.inspect(returned, {
+                            depth: 1
+                        })
+                        if (str.length > 1900) {
+                            str = str.substr(0, 1897)
+                            str = str + '...';
+                        }
+                        ms.edit('```xl\n' + str + '\n```')
+                    }, e => {
+                        str = util.inspect(e, {
+                            depth: 1
+                        })
+                        if (str.length > 1900) {
+                            str = str.substr(0, 1897)
+                            str = str + '...';
+                        }
+                        ms.edit('```xl\n' + str + '\n```')
+                    })
+                }
+            }).catch(() => {})
+        } catch (e) {
+            message.channel.send('```xl\n' + e + '\n```').catch(() => {})
+        }
+    }
+}
