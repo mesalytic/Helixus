2b0932a5d871a65d18568dc3bf4c31d0943d48b6
Lola <43297230+aliciacactus@users.noreply.github.com>
2020-09-28 19:28:26 +0200

    add(Event): added embeds in ready event

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,5 @@
+const { WebhookClient, MessageEmbed } = require("discord.js");
+
 module.exports = async (bot) => {
     bot.user.setPresence({
         status: 'online',
@@ -8,4 +10,14 @@ module.exports = async (bot) => {
     });
 
     bot.logger.info(`Helixus is now running [Shard ${bot.shard.ids[0] + 1}]`)
+    
+    const wb = new WebhookClient(bot.config.webhook.status.id, bot.config.webhook.status.password)
+
+    const embed = new MessageEmbed()
+    .setColor("#008000")
+    .setTitle(`Shard ${bot.shard.ids[0] + 1}/${bot.shard.count}`)
+    .setDescription(`Connected`)
+    .setTimestamp();
+
+    wb.send(embed);
 }
