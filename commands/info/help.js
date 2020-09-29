9a3b68b6b4feea3ca13fdd03709f8b98f36b70d8
Lola <43297230+aliciacactus@users.noreply.github.com>
2020-09-29 21:09:39 +0200

    add(Command): added help command
    yes finally, and it's fucking beautiful

---
+++ commands/info/help.js
@@ -0,0 +1,96 @@
+const {
+    oneLine, stripIndent
+} = require("common-tags");
+const {
+    MessageEmbed
+} = require("discord.js");
+const Command = require("../../structures/Command");
+const {
+    capitalize
+} = require("../../structures/Utils");
+
+
+
+module.exports = class HelpCommand extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'help',
+            aliases: ['h'],
+            usage: 'help [command]',
+            description: oneLine `
+            Displays a list of current commands, sorted by category.
+            You can also give an argument, to get more infos about a specific command.
+            `,
+            type: 'info',
+            examples: ['help', 'help ping']
+        });
+    }
+
+    async run(message, args) {
+        const { INFO, FUN, COLOR, POINTS, MISC, MOD, ADMIN, OWNER } = this.bot.types;
+        this.bot.db.query(`SELECT * FROM Prefixes WHERE guildID='${message.guild.id}'`, (err, prefixes) => {
+            if (err) throw err;
+
+            const embed = new MessageEmbed()
+            let prefix;
+
+            if (prefixes[0]) prefix = prefixes[0].prefix;
+            else prefix = "am!"
+
+            const command = this.bot.commands.get(args[0]) || this.bot.aliases.get(args[0]);
+            if (command && (command.type != OWNER)) {
+                embed
+                    .setTitle(`Command: \`${command.name}\``)
+                    .setDescription(command.description)
+                    .addField('Usage', `\`${prefix}${command.usage}\``, true)
+                    .addField('Type', `\`${capitalize(command.type)}\``, true)
+                    .setFooter(message.member.displayName, message.author.displayAvatarURL({
+                        dynamic: true
+                    }))
+                    .setTimestamp()
+                    .setColor(message.guild.me.displayHexColor);
+
+                if (command.aliases) embed.addField('Aliases', command.aliases.map(c => `\`${c}\``).join(' '));
+                if (command.examples) embed.addField('Examples', command.examples.map(c => `\`${prefix}${c}\``).join('\n'));
+            } else {
+                const commands = {};
+                for (const type of Object.values(this.bot.types)) {
+                    commands[type] = [];
+                }
+
+                const emojiMap = {
+                    [INFO]: `${capitalize(INFO)}`,
+        [FUN]: `${capitalize(FUN)}`,
+        [COLOR]: `${capitalize(COLOR)}`,
+        [POINTS]: `${capitalize(POINTS)}`,
+        [MISC]: `${capitalize(MISC)}`,
+        [MOD]: `${capitalize(MOD)}`,
+        [ADMIN]: `${capitalize(ADMIN)}`,
+        [OWNER]: `${capitalize(OWNER)}`
+                }
+
+                this.bot.commands.forEach(command => {
+                    commands[command.type].push(`\`${command.name}\``);
+                })
+
+                const total = Object.values(commands).reduce((a, b) => a + b.length, 0) - commands[OWNER].length;
+                const size = this.bot.commands.size - commands[OWNER].length;
+
+                embed
+                    .setTitle('Helixus Commands')
+                    .setDescription(stripIndent`
+                    **More informations:** \`${prefix}help [command]\`
+                    `)
+                    .setTimestamp()
+                    .setColor(message.guild.me.displayhexColor)
+
+                    for (const type of Object.values(this.bot.types)) {
+                        if (type === OWNER) continue;
+                        if (commands[type][0])
+                          embed.addField(`**${emojiMap[type]} [${commands[type].length}]**`, commands[type].join(', '));
+                    }
+            }
+            message.channel.send(embed);
+        })
+    }
+}
