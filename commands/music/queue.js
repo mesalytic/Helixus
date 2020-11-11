97b8c04c5869c21aa9bf336163c0027c4206cec8
Lola <43297230+aliciacactus@users.noreply.github.com>
2020-11-11 22:46:41 +0100

    add(QueueCommand): added queue command
    allows you to browse through the music queue, with reaction support

---
+++ commands/music/queue.js
@@ -0,0 +1,90 @@
+const {
+    MessageEmbed
+} = require("discord.js");
+const Command = require("../../structures/Command");
+
+module.exports = class QueueCommand extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'queue',
+            description: 'Displays the whole music queue. Use the reactions to navigate.',
+            usage: 'queue',
+            type: 'music'
+        });
+    }
+
+    async run(message) {
+        const permissions = message.channel.permissionsFor(message.client.user);
+        if (!permissions.has(["MANAGE_MESSAGES", "ADD_REACTIONS"]))
+            return message.reply("Missing Permissions: MANAGE_MESSAGES, ADD_REACTIONS");
+
+        const queue = this.bot.queue.get(message.guild.id);
+        if (!queue) return message.channel.send("❌ - There is nothing playing in this server.");
+
+        let currentPage = 0;
+        const embeds = generateQueueEmbed(message, queue.songs);
+
+        const queueEmbed = await message.channel.send(`**Current Page - ${currentPage + 1}/${embeds.length}**`, embeds[currentPage]);
+
+        try {
+            await queueEmbed.react("⬅️");
+            await queueEmbed.react("⏹");
+            await queueEmbed.react("➡️");
+        } catch (error) {
+            console.error(error);
+            message.channel.send(error.message).catch(console.error);
+        }
+
+        const filter = (reaction, user) => ["⬅️", "⏹", "➡️"].includes(reaction.emoji.name) && message.author.id === user.id;
+        const collector = queueEmbed.createReactionCollector(filter, {
+            time: 60000
+        });
+
+        collector.on("collect", async (reaction, user) => {
+            try {
+                if (reaction.emoji.name === "➡️") {
+                    if (currentPage < embeds.length - 1) {
+                        currentPage++;
+                        queueEmbed.edit(`**Current Page - ${currentPage + 1}/${embeds.length}**`, embeds[currentPage]);
+                    }
+                } else if (reaction.emoji.name === "⬅️") {
+                    if (currentPage !== 0) {
+                        --currentPage;
+                        queueEmbed.edit(`**Current Page - ${currentPage + 1}/${embeds.length}**`, embeds[currentPage]);
+                    }
+                } else {
+                    collector.stop();
+                    reaction.message.reactions.removeAll();
+                }
+                await reaction.users.remove(message.author.id);
+            } catch (error) {
+                console.error(error);
+                return message.channel.send(error.message).catch(console.error);
+            }
+        });
+
+
+        function generateQueueEmbed(msg, queue) {
+            let embeds = [];
+            let k = 10;
+
+            for (let i = 0; i < queue.length; i += 10) {
+                const current = queue.slice(i, k);
+                let j = i;
+                k += 10;
+
+                const info = current.map((track) => `${++j} - [${track.title}](${track.url})`).join("\n");
+
+                const embed = new MessageEmbed()
+                    .setTitle("Song Queue\n")
+                    .setThumbnail(message.guild.iconURL())
+                    .setColor("RANDOM")
+                    .setDescription(`**Current Song - [${queue[0].title}](${queue[0].url})**\n\n${info}`)
+                    .setTimestamp();
+                embeds.push(embed);
+            }
+
+            return embeds;
+        }
+    }
+}
