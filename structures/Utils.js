f259f5cb4b1621f5ea862f8e3386f93f6865229b
Lola <43297230+aliciacactus@users.noreply.github.com>
2020-11-29 03:02:12 +0100

    add(Utils): added compareArrays function

--- structures/Utils.js
+++ structures/Utils.js
@@ -41,9 +41,20 @@ function canModifyQueue(member) {
 
     return true;
 }
+function compareArrays (a, b) {
+    if (a === b) return true
+    if (a == null || b == null) return false
+    if (a.length !== b.length) return false
+  
+    for (let i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false
+    }
+    return true
+  }
 
 module.exports = {
     capitalize,
     timeZoneConvert,
-    canModifyQueue
+    canModifyQueue,
+    compareArrays
 };
