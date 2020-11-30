c3009b2766440d975178e8c653f43978739d3265
MesaVirep <atamatori56@gmail.com>
2020-11-30 22:17:14 +0100

    add(MessageDeleteEvent): added messageDelete log event

---
+++ events/messageDelete.js
@@ -0,0 +1,76 @@
+const {
+    WebhookClient,
+    MessageEmbed
+} = require("discord.js")
+
+const hastebin = require("hastebin-gen");
+
+const moment = require('moment');
+
+module.exports = async (bot, message) => {
+    bot.db.query(`SELECT * FROM Logs WHERE guildID='${message.guild.id}'`, async (err, logsSettings) => {
+        if (err) throw err;
+        if (logsSettings[0]) {
+            if (logsSettings[0].channelID) {
+                if (logsSettings[0].activated = "true") {
+                    if (logsSettings[0].messagedelete === "true") {
+                        if (logsSettings[0].webhookID && logsSettings[0].webhookToken) {
+                            const webhook = new WebhookClient(logsSettings[0].webhookID, logsSettings[0].webhookToken);
+                            
+                            let embed = new MessageEmbed()
+                                .setAuthor(message.author.tag, message.author.avatarURL())
+                                .setDescription(`A message from ${message.author} (${message.author.tag}) has been deleted.`)
+                                .addField("Deleted by", `Unknown`, true)
+                                .setColor("RANDOM")
+                                .setTimestamp();
+
+                                if (message.attachments.first()) {
+                                    if (!message.content) embed.addField("Attachment", message.attachments.first().proxyURL);
+                                    else {
+                                        if (message.content.length > 1023) {
+                                            embed.addField("Message Content", await hastebin(message.content));
+                                            embed.addField("Attachment", message.attachments.first().proxyURL);
+                                        } else {
+                                            embed.addField("Message Content", message.content);
+                                            embed.addField("Attachment", message.attachments.first().proxyURL);
+                                        }
+                                    }
+                                } else {
+                                    if (message.content.length > 1023) embed.addField("Message Content", await hastebin(message.content));
+                                    else embed.addField("Message Content", message.content);
+                                }
+                            
+                            
+                            
+                                await setTimeout(async () => {
+                                    const logs = await message.guild.fetchAuditLogs({
+                                        limit: 5,
+                                        type: 72
+                                    }).catch(() => {
+                                        return
+                                    });
+                                    if (!logs) return;
+    
+                                    const log = logs.entries.first()
+                                    const executor = message.guild.members.cache.get(log.executor.id);
+    
+                                    if (new Date().getTime() - new Date((log.id / 4194304) + 1420070400000).getTime() < 3000) {
+                                        embed.fields[0].value = executor
+                                        console.log(embed.fields[0].value)
+                                        if (embed.fields[0].value === "Unknown") embed.fields[0].value = message.author
+                                        
+                                        await webhook.send(embed);
+                                    } else {
+                                        console.log(embed.fields[0].value)
+                                        if (embed.fields[0].value === "Unknown") embed.fields[0].value = message.author
+                                        
+                                        await webhook.send(embed);
+                                    }
+                                }, 1000)
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
