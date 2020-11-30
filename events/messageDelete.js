9dc4a0c6da85dfbcd6e2fa684df662f379d584cd
MesaVirep <atamatori56@gmail.com>
2020-12-01 00:25:24 +0100

    add(MessageDeleteEvent): added support for LogsIgnore
    LogsIgnore will come later

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -14,60 +14,64 @@ module.exports = async (bot, message) => {
             if (logsSettings[0].channelID) {
                 if (logsSettings[0].activated = "true") {
                     if (logsSettings[0].messagedelete === "true") {
-                        if (logsSettings[0].webhookID && logsSettings[0].webhookToken) {
-                            const webhook = new WebhookClient(logsSettings[0].webhookID, logsSettings[0].webhookToken);
-                            
-                            let embed = new MessageEmbed()
-                                .setAuthor(message.author.tag, message.author.avatarURL())
-                                .setDescription(`A message from ${message.author} (${message.author.tag}) has been deleted.`)
-                                .addField("Deleted by", `Unknown`, true)
-                                .setColor("RANDOM")
-                                .setTimestamp();
-
-                                if (message.attachments.first()) {
-                                    if (!message.content) embed.addField("Attachment", message.attachments.first().proxyURL);
-                                    else {
-                                        if (message.content.length > 1023) {
-                                            embed.addField("Message Content", await hastebin(message.content));
-                                            embed.addField("Attachment", message.attachments.first().proxyURL);
+                        bot.db.query(`SELECT * FROM LogsIgnore WHERE guildID='${message.guild.id}' AND channelID='${message.channel.id}'`, async (err, ignore) => {
+                            if (!ignore[0] || ignore[0].ignored === "false") {
+                                if (logsSettings[0].webhookID && logsSettings[0].webhookToken) {
+                                    const webhook = new WebhookClient(logsSettings[0].webhookID, logsSettings[0].webhookToken);
+                                    
+                                    let embed = new MessageEmbed()
+                                        .setAuthor(message.author.tag, message.author.avatarURL())
+                                        .setDescription(`A message from ${message.author} (${message.author.tag}) has been deleted.`)
+                                        .addField("Deleted by", `Unknown`, true)
+                                        .setColor("RANDOM")
+                                        .setTimestamp();
+        
+                                        if (message.attachments.first()) {
+                                            if (!message.content) embed.addField("Attachment", message.attachments.first().proxyURL);
+                                            else {
+                                                if (message.content.length > 1023) {
+                                                    embed.addField("Message Content", await hastebin(message.content));
+                                                    embed.addField("Attachment", message.attachments.first().proxyURL);
+                                                } else {
+                                                    embed.addField("Message Content", message.content);
+                                                    embed.addField("Attachment", message.attachments.first().proxyURL);
+                                                }
+                                            }
                                         } else {
-                                            embed.addField("Message Content", message.content);
-                                            embed.addField("Attachment", message.attachments.first().proxyURL);
+                                            if (message.content.length > 1023) embed.addField("Message Content", await hastebin(message.content));
+                                            else embed.addField("Message Content", message.content);
                                         }
-                                    }
-                                } else {
-                                    if (message.content.length > 1023) embed.addField("Message Content", await hastebin(message.content));
-                                    else embed.addField("Message Content", message.content);
+                                    
+                                    
+                                    
+                                        await setTimeout(async () => {
+                                            const logs = await message.guild.fetchAuditLogs({
+                                                limit: 5,
+                                                type: 72
+                                            }).catch(() => {
+                                                return
+                                            });
+                                            if (!logs) return;
+            
+                                            const log = logs.entries.first()
+                                            const executor = message.guild.members.cache.get(log.executor.id);
+            
+                                            if (new Date().getTime() - new Date((log.id / 4194304) + 1420070400000).getTime() < 3000) {
+                                                embed.fields[0].value = executor
+                                                console.log(embed.fields[0].value)
+                                                if (embed.fields[0].value === "Unknown") embed.fields[0].value = message.author
+                                                
+                                                await webhook.send(embed);
+                                            } else {
+                                                console.log(embed.fields[0].value)
+                                                if (embed.fields[0].value === "Unknown") embed.fields[0].value = message.author
+                                                
+                                                await webhook.send(embed);
+                                            }
+                                        }, 1000)
                                 }
-                            
-                            
-                            
-                                await setTimeout(async () => {
-                                    const logs = await message.guild.fetchAuditLogs({
-                                        limit: 5,
-                                        type: 72
-                                    }).catch(() => {
-                                        return
-                                    });
-                                    if (!logs) return;
-    
-                                    const log = logs.entries.first()
-                                    const executor = message.guild.members.cache.get(log.executor.id);
-    
-                                    if (new Date().getTime() - new Date((log.id / 4194304) + 1420070400000).getTime() < 3000) {
-                                        embed.fields[0].value = executor
-                                        console.log(embed.fields[0].value)
-                                        if (embed.fields[0].value === "Unknown") embed.fields[0].value = message.author
-                                        
-                                        await webhook.send(embed);
-                                    } else {
-                                        console.log(embed.fields[0].value)
-                                        if (embed.fields[0].value === "Unknown") embed.fields[0].value = message.author
-                                        
-                                        await webhook.send(embed);
-                                    }
-                                }, 1000)
-                        }
+                            }
+                        })
                     }
                 }
             }
