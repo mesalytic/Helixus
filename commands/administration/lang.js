346a70a160ad9ae1dc9a87cf154f72aab92dfc6b
MesaVirep <atamatori56@gmail.com>
2020-12-13 01:04:08 +0100

    add(commands#lang): added lang command
    and soon the translated files + the languages files...

---
+++ commands/administration/lang.js
@@ -0,0 +1,33 @@
+const {
+    MessageEmbed
+} = require("discord.js");
+const Command = require("../../structures/Command");
+
+module.exports = class LangCommand extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'lang',
+            usage: 'lang <fr/en>',
+            description: 'Changes the bot\'s language on the server.',
+            type: 'administration',
+            userPermissions: ["MANAGE_GUILD"]
+        });
+    }
+
+    async run(message, args) {
+  if (!args[0]) return this.bot.commands.get("help").run(message, ["lang"]);
+
+  if (["en", "fr"].includes(args[0].toLowerCase())) {
+    this.bot.db.query(`SELECT * FROM Langs WHERE guildID='${message.guild.id}'`, (err, rows) => {
+      if (!rows[0]) {
+        this.bot.db.query(`INSERT INTO Langs (guildID, lang) VALUES ('${message.guild.id}', '${args[0].toLowerCase()}')`);
+      } else {
+        this.bot.db.query(`UPDATE Langs SET lang = '${args[0].toLowerCase()}' WHERE guildID='${message.guild.id}'`);
+      }
+      message.channel.send(`[V] - The server language has been updated to **${args[0].toLowerCase()}**`);
+    });
+  } else {
+      return this.bot.commands.get("help").run(message, ["lang"]);
+  }
+    }
+}
