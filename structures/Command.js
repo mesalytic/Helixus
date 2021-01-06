f545782111f08bc6465dc97da0f8a2a4da3f2711
MesaVirep <atamatori56@gmail.com>
2021-01-06 22:38:12 +0100

    perf(Command): cooldown is now in seconds instead of milliseconds

--- structures/Command.js
+++ structures/Command.js
@@ -17,7 +17,7 @@ module.exports = class Command {
 		this.examples = options.examples || null;
 		this.ownerOnly = options.ownerOnly || false;
 		this.disabled = options.disabled || false;
-		this.cooldown = options.cooldown || 3000;
+		this.cooldown = options.cooldown * 1000 || 3000;
 		this.errorTypes = ['Invalid Argument', 'Command Failure'];
 	}
 
