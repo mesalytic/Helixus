9fca7c15c8d6ac75ce32fb1e2a2c5b6b7c007a0e
MesaVirep <atamatori56@gmail.com>
2021-03-15 21:40:44 +0100

    fix: coins are given only when message is not a command

--- events/message.js
+++ events/message.js
@@ -123,6 +123,146 @@ module.exports = (bot, message) => {
           return message.reply(message.guild.lang.EVENTS.MESSAGE.error(e))
         }
 
+      } else {
+        function generateXP(min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+        bot.db.query(`SELECT * FROM LevelsConfig WHERE guildID='${message.guild.id}'`, (err, rows) => {
+          if (rows[0]) {
+            if (rows[0].activated === "true") {
+              bot.db.query(`SELECT * FROM Cooldowns WHERE userID='${message.author.id}'`, (err, cRows) => {
+                if (!cRows[0]) bot.db.query(`INSERT INTO Cooldowns (userID, guildID, active) VALUES ('${message.author.id}', '${message.guild.id}', 'true')`);
+                else return setTimeout(() => {
+                  bot.db.query(`DELETE FROM Cooldowns WHERE userID='${message.author.id}'`);
+                }, 60 * 1000);
+    
+                bot.db.query(`SELECT * FROM Levels WHERE guild='${message.guild.id}' AND user='${message.author.id}'`, (err, lRows) => {
+                  if (err) throw err;
+    
+                  if (!lRows[0]) {
+                    bot.db.query(`INSERT INTO Levels (user, guild, points, level) VALUES ('${message.author.id}', '${message.guild.id}', '${generateXP(5, 15)}', '1')`);
+                  } else {
+    
+                    if (!message.guild.lang) {
+                      bot.db.query(`SELECT * FROM Langs WHERE guildID='${message.guild.id}'`, (err, rows) => {
+                        message.guild.lang = require(`../structures/Languages/${rows[0] ? rows[0].lang : "en"}.js`);
+                      })
+                    }
+    
+                    let xp;
+                    let xpToWin = generateXP(5, 15);
+    
+                    if (!lRows[0]) xp = 0;
+                    else xp = Number(lRows[0].points);
+    
+                    let xpFuture = lRows[0].points + xpToWin;
+                    let cLvl = (5 * (lRows[0].level ^ 2) + 50 * lRows[0].level + 100) * 1.20
+                    if (xpFuture >= cLvl) {
+                      bot.db.query(`UPDATE Levels SET level = '${Number(lRows[0].level) + 1}', points = '0' WHERE guild='${message.guild.id}' AND user='${message.author.id}'`);
+    
+                      if (!rows[0].lvlupChannelID || rows[0].lvlupChannelID === "msgChannel") channel = message.channel.id;
+                      else channel = rows[0].lvlupChannelID;
+    
+                      if (!rows[0].lvlupMessage) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
+                      else lvlupMsg = rows[0].lvlupMessage;
+    
+                      bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${lRows[0].level + 1}'`, (err, rRows) => {
+                        if (rRows[0]) {
+                          const role = message.guild.roles.resolve(rRows[0].roleID);
+                          if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
+                        }
+                      });
+                      for (let i = 0; i < Number(lRows[0].level); i++) {
+                        bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${i + 1}'`, (err, rRows) => {
+                          if (rRows[0]) {
+                            const role = message.guild.roles.resolve(rRows[0].roleID);
+                            if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
+                          }
+                        });
+                      }
+    
+                      if (!channel) channel = message.channel.id;
+                      if (!lvlupMsg) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
+    
+                      const res = lvlupMsg.replace(/{user}/g, message.author).replace(/{level}/g, Number(lRows[0].level + 1)).replace(/{server}/g, message.guild.name).replace(/{username}/g, message.author.username);
+    
+                      let chan = bot.channels.cache.get(channel);
+                      if (!chan) return;
+                      chan.send(res).catch(() => {});
+                    } else bot.db.query(`UPDATE Levels SET points = '${lRows[0].points + xpToWin}' WHERE guild='${message.guild.id}' AND user='${message.author.id}'`);
+                  }
+                })
+              })
+            }
+          }
+        })
+        bot.db.query(`SELECT * FROM LevelsConfig WHERE guildID='${message.guild.id}'`, (err, rows) => {
+          if (rows[0]) {
+            if (rows[0].activated === "true") {
+    
+    
+    
+              bot.db.query(`SELECT * FROM Levels WHERE guild='${message.guild.id}' AND user='${message.author.id}'`, (err, lRows) => {
+                var channel;
+                var lvlupMsg;
+    
+                if (!lRows[0]) return;
+                if (!Number(lRows[0].points)) return;
+                const clvl = (5 * (lRows[0].level ^ 2) + 50 * lRows[0].level + 100) * 1.20;
+    
+                if (Number(lRows[0].points) >= clvl) {
+                  bot.db.query(`UPDATE Levels SET level = '${Number(lRows[0].level) + 1}', points = '0' WHERE guild='${message.guild.id}' AND user='${message.author.id}'`);
+    
+                  if (!rows[0].lvlupChannelID || rows[0].lvlupChannelID === "msgChannel") channel = message.channel.id;
+                  else channel = rows[0].lvlupChannelID;
+    
+                  if (!rows[0].lvlupMessage) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
+                  else lvlupMsg = rows[0].lvlupMessage;
+    
+                  bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${lRows[0].level + 1}'`, (err, rRows) => {
+                    if (rRows[0]) {
+                      const role = message.guild.roles.resolve(rRows[0].roleID);
+                      if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
+                    }
+                  });
+                  for (let i = 0; i < Number(lRows[0].level); i++) {
+                    bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${i + 1}'`, (err, rRows) => {
+                      if (rRows[0]) {
+                        const role = message.guild.roles.resolve(rRows[0].roleID);
+                        if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
+                      }
+                    });
+                  }
+    
+                  if (!channel) channel = message.channel.id;
+                  if (!lvlupMsg) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
+    
+                  const res = lvlupMsg.replace(/{user}/g, message.author).replace(/{level}/g, Number(lRows[0].level + 1)).replace(/{server}/g, message.guild.name).replace(/{username}/g, message.author.username);
+    
+                  let chan = bot.channels.cache.get(channel);
+                  if (!chan) return;
+                  chan.send(res).catch(() => {});
+                }
+              })
+            }
+          }
+        })
+    
+    
+        bot.db.query(`SELECT * FROM Economy WHERE userID='${message.author.id}'`, (err, rows) => {
+          bot.db.query(`SELECT * FROM Cooldowns WHERE userID='${message.author.id}' AND type='coins'`, (err, cools) => {
+            if (!cools[0]) bot.db.query(`INSERT INTO Cooldowns (userID, guildID, active, type) VALUES ('${message.author.id}', '${message.guild.id}', 'true', 'coins')`);
+            else return setTimeout(() => {
+              bot.db.query(`DELETE FROM Cooldowns WHERE userID='${message.author.id}' AND type='coins'`);
+            }, 60 * 1000);
+    
+            if (!rows[0]) {
+              bot.db.query(`INSERT INTO Economy (userID, balance) VALUES ('${message.author.id}', '${generateXP(5, 15)}')`);
+            } else {
+              bot.db.query(`UPDATE Economy SET balance = '${rows[0].balance + generateXP(5, 15)}' WHERE userID='${message.author.id}'`);
+            }
+          })
+        })
       }
     } else {
       bot.db.query(`SELECT * FROM Afk WHERE userID = '${message.author.id}'`, (err, rows) => {
@@ -144,144 +284,6 @@ module.exports = (bot, message) => {
 
     /* Levels */
 
-    function generateXP(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    bot.db.query(`SELECT * FROM LevelsConfig WHERE guildID='${message.guild.id}'`, (err, rows) => {
-      if (rows[0]) {
-        if (rows[0].activated === "true") {
-          bot.db.query(`SELECT * FROM Cooldowns WHERE userID='${message.author.id}'`, (err, cRows) => {
-            if (!cRows[0]) bot.db.query(`INSERT INTO Cooldowns (userID, guildID, active) VALUES ('${message.author.id}', '${message.guild.id}', 'true')`);
-            else return setTimeout(() => {
-              bot.db.query(`DELETE FROM Cooldowns WHERE userID='${message.author.id}'`);
-            }, 60 * 1000);
-
-            bot.db.query(`SELECT * FROM Levels WHERE guild='${message.guild.id}' AND user='${message.author.id}'`, (err, lRows) => {
-              if (err) throw err;
-
-              if (!lRows[0]) {
-                bot.db.query(`INSERT INTO Levels (user, guild, points, level) VALUES ('${message.author.id}', '${message.guild.id}', '${generateXP(5, 15)}', '1')`);
-              } else {
-
-                if (!message.guild.lang) {
-                  bot.db.query(`SELECT * FROM Langs WHERE guildID='${message.guild.id}'`, (err, rows) => {
-                    message.guild.lang = require(`../structures/Languages/${rows[0] ? rows[0].lang : "en"}.js`);
-                  })
-                }
-
-                let xp;
-                let xpToWin = generateXP(5, 15);
-
-                if (!lRows[0]) xp = 0;
-                else xp = Number(lRows[0].points);
-
-                let xpFuture = lRows[0].points + xpToWin;
-                let cLvl = (5 * (lRows[0].level ^ 2) + 50 * lRows[0].level + 100) * 1.20
-                if (xpFuture >= cLvl) {
-                  bot.db.query(`UPDATE Levels SET level = '${Number(lRows[0].level) + 1}', points = '0' WHERE guild='${message.guild.id}' AND user='${message.author.id}'`);
-
-                  if (!rows[0].lvlupChannelID || rows[0].lvlupChannelID === "msgChannel") channel = message.channel.id;
-                  else channel = rows[0].lvlupChannelID;
-
-                  if (!rows[0].lvlupMessage) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
-                  else lvlupMsg = rows[0].lvlupMessage;
-
-                  bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${lRows[0].level + 1}'`, (err, rRows) => {
-                    if (rRows[0]) {
-                      const role = message.guild.roles.resolve(rRows[0].roleID);
-                      if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
-                    }
-                  });
-                  for (let i = 0; i < Number(lRows[0].level); i++) {
-                    bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${i + 1}'`, (err, rRows) => {
-                      if (rRows[0]) {
-                        const role = message.guild.roles.resolve(rRows[0].roleID);
-                        if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
-                      }
-                    });
-                  }
-
-                  if (!channel) channel = message.channel.id;
-                  if (!lvlupMsg) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
-
-                  const res = lvlupMsg.replace(/{user}/g, message.author).replace(/{level}/g, Number(lRows[0].level + 1)).replace(/{server}/g, message.guild.name).replace(/{username}/g, message.author.username);
-
-                  let chan = bot.channels.cache.get(channel);
-                  if (!chan) return;
-                  chan.send(res).catch(() => {});
-                } else bot.db.query(`UPDATE Levels SET points = '${lRows[0].points + xpToWin}' WHERE guild='${message.guild.id}' AND user='${message.author.id}'`);
-              }
-            })
-          })
-        }
-      }
-    })
-    bot.db.query(`SELECT * FROM LevelsConfig WHERE guildID='${message.guild.id}'`, (err, rows) => {
-      if (rows[0]) {
-        if (rows[0].activated === "true") {
-
-
-
-          bot.db.query(`SELECT * FROM Levels WHERE guild='${message.guild.id}' AND user='${message.author.id}'`, (err, lRows) => {
-            var channel;
-            var lvlupMsg;
-
-            if (!lRows[0]) return;
-            if (!Number(lRows[0].points)) return;
-            const clvl = (5 * (lRows[0].level ^ 2) + 50 * lRows[0].level + 100) * 1.20;
-
-            if (Number(lRows[0].points) >= clvl) {
-              bot.db.query(`UPDATE Levels SET level = '${Number(lRows[0].level) + 1}', points = '0' WHERE guild='${message.guild.id}' AND user='${message.author.id}'`);
-
-              if (!rows[0].lvlupChannelID || rows[0].lvlupChannelID === "msgChannel") channel = message.channel.id;
-              else channel = rows[0].lvlupChannelID;
-
-              if (!rows[0].lvlupMessage) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
-              else lvlupMsg = rows[0].lvlupMessage;
-
-              bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${lRows[0].level + 1}'`, (err, rRows) => {
-                if (rRows[0]) {
-                  const role = message.guild.roles.resolve(rRows[0].roleID);
-                  if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
-                }
-              });
-              for (let i = 0; i < Number(lRows[0].level); i++) {
-                bot.db.query(`SELECT * FROM LevelsRewards WHERE guildID='${message.guild.id}' AND level='${i + 1}'`, (err, rRows) => {
-                  if (rRows[0]) {
-                    const role = message.guild.roles.resolve(rRows[0].roleID);
-                    if (!message.member.roles.cache.has(role)) message.member.roles.add(role);
-                  }
-                });
-              }
-
-              if (!channel) channel = message.channel.id;
-              if (!lvlupMsg) lvlupMsg = message.guild.lang.EVENTS.MESSAGE.lvlUpMessage;
-
-              const res = lvlupMsg.replace(/{user}/g, message.author).replace(/{level}/g, Number(lRows[0].level + 1)).replace(/{server}/g, message.guild.name).replace(/{username}/g, message.author.username);
-
-              let chan = bot.channels.cache.get(channel);
-              if (!chan) return;
-              chan.send(res).catch(() => {});
-            }
-          })
-        }
-      }
-    })
-
-
-    bot.db.query(`SELECT * FROM Economy WHERE userID='${message.author.id}'`, (err, rows) => {
-      bot.db.query(`SELECT * FROM Cooldowns WHERE userID='${message.author.id}' AND type='coins'`, (err, cools) => {
-        if (!cools[0]) bot.db.query(`INSERT INTO Cooldowns (userID, guildID, active, type) VALUES ('${message.author.id}', '${message.guild.id}', 'true', 'coins')`);
-        else return setTimeout(() => {
-          bot.db.query(`DELETE FROM Cooldowns WHERE userID='${message.author.id}' AND type='coins'`);
-        }, 60 * 1000);
-
-        if (!rows[0]) {
-          bot.db.query(`INSERT INTO Economy (userID, balance) VALUES ('${message.author.id}', '${generateXP(5, 15)}')`);
-        } else {
-          bot.db.query(`UPDATE Economy SET balance = '${rows[0].balance + generateXP(5, 15)}' WHERE userID='${message.author.id}'`);
-        }
-      })
-    })
+    
   })
 }
