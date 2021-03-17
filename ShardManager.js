ab733684b4fdac86050b9bb7fbed7fb0c28918ce
MesaVirep <atamatori56@gmail.com>
2021-03-17 23:13:50 +0100

    feat: added topGG integration

--- ShardManager.js
+++ ShardManager.js
@@ -1,9 +1,19 @@
 const { ShardingManager } = require('discord.js');
 const config = require('./config.json');
 
+const os = require('os');
+
 const Manager = new ShardingManager("./index.js", {
     totalShards: "auto",
     token: config.token,
     shardArgs: ["--trace-warning"],
 });
+
+if (os.platform() === "win32") { /* */ }
+else {
+    const AutoPoster = require('topgg-autoposter');
+
+    const ap = AutoPoster(config.topgg, Manager);
+}
+
 Manager.spawn();
