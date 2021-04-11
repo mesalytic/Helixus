792e09d7b122bcaa64ca192f2ac25fe7e96875a0
chocololat <atamatori56@gmail.com>
2021-04-11 21:24:16 +0200

    test(GuildCreate): testing bugfix for server owner fetch

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,19 +6,21 @@ const {
 module.exports = async (bot, guild) => {
     const wb = new WebhookClient(bot.config.webhook.joinleaves.id, bot.config.webhook.joinleaves.password);
 
-    const promises = [bot.shard.fetchClientValues("guilds.cache.size")];
+    guild.members.fetch().then(() => {
+        const promises = [bot.shard.fetchClientValues("guilds.cache.size")];
 
-    Promise.all(promises).then((res) => {
-        const guilds = res[0].reduce((prev, guild) => prev + guild, 0);
-
-        let e = new MessageEmbed()
-            .setColor("RANDOM")
-            .setTitle("**A server added the bot!**")
-            .setDescription(`Server: **${guild.name}** (\`${guild.id}\`)\nMade by **${guild.owner.user.tag}** (\`${guild.owner.id}\`)\nMembers: **${guild.memberCount}**\n\nI am now in **${guilds}** guilds!`)
-            .setThumbnail(guild.iconURL({
-                size: 256
-            }))
-            .setTimestamp();
-        wb.send(e);
+        Promise.all(promises).then((res) => {
+            const guilds = res[0].reduce((prev, guild) => prev + guild, 0);
+    
+            let e = new MessageEmbed()
+                .setColor("RANDOM")
+                .setTitle("**A server added the bot!**")
+                .setDescription(`Server: **${guild.name}** (\`${guild.id}\`)\nMade by **${guild.owner.user.tag}** (\`${guild.owner.id}\`)\nMembers: **${guild.memberCount}**\n\nI am now in **${guilds}** guilds!`)
+                .setThumbnail(guild.iconURL({
+                    size: 256
+                }))
+                .setTimestamp();
+            wb.send(e);
+        })    
     })
 }
