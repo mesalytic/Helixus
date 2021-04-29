554132a006ee8d89e97e59b018dfd961283efcd0
chocololat <atamatori56@gmail.com>
2021-04-29 16:25:23 +0200

    feat(Daily): add premium functionality

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -11,22 +11,23 @@ module.exports = class DailyCommand extends Command {
         });
     }
 
-    async run(message, args) {
+    async run(message) {
         this.bot.db.query(`SELECT * FROM Economy WHERE userID='${message.author.id}'`, (err, rows) => {
-
-            let timeout = 86400000;
-            let amount = 200;
-
-            if (rows[0] && (rows[0].dailyCooldown !== null && timeout - (Date.now() - rows[0].dailyCooldown) > 0)) {
-                
-                let time = ms(timeout - (Date.now() - rows[0].dailyCooldown));
-                return message.reply(message.guild.lang.COMMANDS.DAILY.notReady(time))
-            } else {
-                if (!rows[0]) this.bot.db.query(`INSERT INTO Economy (userID, balance, dailyCooldown) VALUES ('${message.author.id}', '${amount}', '${Date.now()}')`)
-                else this.bot.db.query(`UPDATE Economy SET balance = '${rows[0].balance + amount}', dailyCooldown = '${Date.now()}' WHERE userID='${message.author.id}'`);
-
-                return message.reply(message.guild.lang.COMMANDS.DAILY.success(amount));
-            }
+            this.bot.db.query(`SELECT * FROM userPremiums WHERE premiumHolder='${message.author.id}'`, (err, premiums) => {
+                let timeout = 86400000;
+                let amount = premiums[0] ? 400 : 200;
+    
+                if (rows[0] && (rows[0].dailyCooldown !== null && timeout - (Date.now() - rows[0].dailyCooldown) > 0)) {
+                    
+                    let time = ms(timeout - (Date.now() - rows[0].dailyCooldown));
+                    return message.reply(message.guild.lang.COMMANDS.DAILY.notReady(time))
+                } else {
+                    if (!rows[0]) this.bot.db.query(`INSERT INTO Economy (userID, balance, dailyCooldown) VALUES ('${message.author.id}', '${amount}', '${Date.now()}')`)
+                    else this.bot.db.query(`UPDATE Economy SET balance = '${rows[0].balance + amount}', dailyCooldown = '${Date.now()}' WHERE userID='${message.author.id}'`);
+    
+                    return message.reply(`${message.guild.lang.COMMANDS.DAILY.success(amount)} ${premiums[0] ? "*x2 coins (Premium)*" : ""}`);
+                }
+            })
         })
     }
 }
