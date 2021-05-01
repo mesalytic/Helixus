a20f9963d095954fc5aed631826a8890ddf187b9
chocololat <atamatori56@gmail.com>
2021-05-01 17:49:59 +0200

    feat(Work): add premium perks

--- commands/economy/cooldowns.js
+++ commands/economy/cooldowns.js
@@ -15,24 +15,26 @@ module.exports = class CooldownsCommand extends Command {
 
     async run(message) {
         this.bot.db.query(`SELECT * FROM Economy WHERE userID='${message.author.id}'`, (err, rows) => {
-            if (err) throw err;
+            this.bot.db.query(`SELECT * FROM userPremiums WHERE premiumHolder='${message.author.id}'`, (err, premiums) => {
+                if (err) throw err;
 
-            let dailyTime = ms(86400000 - (Date.now() - rows[0].dailyCooldown));
-            let begTime = ms(900000 - (Date.now() - rows[0].begCooldown));
-            let weeklyTime = ms(604800000 - (Date.now() - rows[0].weeklyCooldown));
-            let workTime = ms(43200000 - (Date.now() - rows[0].workCooldown));
-            let voteTime = ms(43200000 - (Date.now() - rows[0].voteCooldown));
-            
-            let embed = new MessageEmbed()
-                .setColor("RANDOM")
-                .setTitle(`${message.author.tag}'s cooldowns.`)
-                .setDescription(`${dailyTime.seconds < 0 ? "✅" : "⌛"} am!daily: ${dailyTime.seconds < 0 ? message.guild.lang.COMMANDS.COOLDOWNS.available : `**${dailyTime.hours}:${dailyTime.minutes}:${dailyTime.seconds}**`}
+                let dailyTime = ms(86400000 - (Date.now() - rows[0].dailyCooldown));
+                let begTime = ms(900000 - (Date.now() - rows[0].begCooldown));
+                let weeklyTime = ms(604800000 - (Date.now() - rows[0].weeklyCooldown));
+                let workTime = premiums[0] && premiums[0].activated ? ms(28800000 - (Date.now() - rows[0].workCooldown)) : ms(43200000 - (Date.now() - rows[0].workCooldown));
+                let voteTime = ms(43200000 - (Date.now() - rows[0].voteCooldown));
+
+                let embed = new MessageEmbed()
+                    .setColor("RANDOM")
+                    .setTitle(`${message.author.tag}'s cooldowns.`)
+                    .setDescription(`${dailyTime.seconds < 0 ? "✅" : "⌛"} am!daily: ${dailyTime.seconds < 0 ? message.guild.lang.COMMANDS.COOLDOWNS.available : `**${dailyTime.hours}:${dailyTime.minutes}:${dailyTime.seconds}**`}
                 ${begTime.seconds < 0 ? "✅" : "⌛"} am!beg: ${begTime.seconds < 0 ? message.guild.lang.COMMANDS.COOLDOWNS.available : `**${begTime.minutes}:${begTime.seconds}**`}
                 ${weeklyTime.seconds < 0 ? "✅" : "⌛"} am!weekly: ${weeklyTime.seconds < 0 ? message.guild.lang.COMMANDS.COOLDOWNS.available : `**${weeklyTime.days}:${weeklyTime.hours}:${weeklyTime.minutes}:${weeklyTime.seconds}**`}
                 ${workTime.seconds < 0 ? "✅" : "⌛"} am!work: ${workTime.seconds < 0 ? message.guild.lang.COMMANDS.COOLDOWNS.available : `**${workTime.hours}:${workTime.minutes}:${workTime.seconds}**`}
                 ${voteTime.seconds < 0 ? "✅" : "⌛"} am!vote: ${voteTime.seconds < 0 ? message.guild.lang.COMMANDS.COOLDOWNS.available : `**${voteTime.hours}:${voteTime.minutes}:${voteTime.seconds}**`}`)
 
-            message.channel.send(embed);
+                message.channel.send(embed);
+            })
         })
     }
 }
--- commands/economy/work.js
+++ commands/economy/work.js
@@ -14,22 +14,24 @@ module.exports = class WorkCommand extends Command {
 
     async run(message) {
         this.bot.db.query(`SELECT * FROM Economy WHERE userID='${message.author.id}'`, (err, rows) => {
-            if (err) throw err;
+            this.bot.db.query(`SELECT * FROM userPremiums WHERE premiumHolder='${message.author.id}'`, (err, premiums) => {
+                if (err) throw err;
 
-            let timeout = 43200000;
-            let money = Math.floor(Math.random() * 500) + 1;
+                let timeout = premiums[0] && premiums[0].activated === "true" ? 28800000 : 43200000;
+                let money = Math.floor(Math.random() * 500) + 1;
 
-            if (rows[0] && (rows[0].workCooldown !== null && timeout - (Date.now() - rows[0].workCooldown) > 0)) {
+                if (rows[0] && (rows[0].workCooldown !== null && timeout - (Date.now() - rows[0].workCooldown) > 0)) {
 
-                let time = ms(timeout - (Date.now() - rows[0].workCooldown));
-                return message.reply(message.guild.lang.COMMANDS.WORK.cooldown(time))
-            } else {
-                let job = jobs[message.guild.lang.code][Math.floor(Math.random() * jobs[message.guild.lang.code].length)];
+                    let time = ms(timeout - (Date.now() - rows[0].workCooldown));
+                    return message.reply(message.guild.lang.COMMANDS.WORK.cooldown(time))
+                } else {
+                    let job = jobs[message.guild.lang.code][Math.floor(Math.random() * jobs[message.guild.lang.code].length)];
 
-                if (!rows[0]) this.bot.db.query(`INSERT INTO Economy (userID, balance, workCooldown) VALUES ('${message.author.id}', '${money}', '${Date.now()}')`)
-                else this.bot.db.query(`UPDATE Economy SET balance='${Number(rows[0].balance) + money}', workCooldown='${Date.now()}' WHERE userID='${message.author.id}'`)
-                message.channel.send(message.guild.lang.COMMANDS.WORK.worked(job, money));
-            }
+                    if (!rows[0]) this.bot.db.query(`INSERT INTO Economy (userID, balance, workCooldown) VALUES ('${message.author.id}', '${money}', '${Date.now()}')`)
+                    else this.bot.db.query(`UPDATE Economy SET balance='${Number(rows[0].balance) + money}', workCooldown='${Date.now()}' WHERE userID='${message.author.id}'`)
+                    message.channel.send(message.guild.lang.COMMANDS.WORK.worked(job, money));
+                }
+            })
         })
     }
 }
