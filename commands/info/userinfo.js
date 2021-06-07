ac9d7bd52157f68b3c735bb5a80f94035590d1da
chocololat <atamatori56@gmail.com>
2021-06-07 21:52:12 +0200

    fix(UserInfo): mentionned user wouldn't work

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -23,7 +23,7 @@ module.exports = class UserInfoCommand extends Command {
     }
 
     async run(message, args) {
-        const guildUsers = message.guild.users;
+        const guildUsers = message.guild.members;
         let user;
 
         if (!args[0] || !message.content.match(/\d{17,19}/)) user = message.member.user;
@@ -35,22 +35,22 @@ module.exports = class UserInfoCommand extends Command {
 
             let embed = new MessageEmbed()
                 .setColor(member && member.displayColor ? member.displayColor : "RANDOM")
-                .setAuthor(user.tag, user.displayAvatarURL({
+                .setAuthor(member.user.tag, member.user.displayAvatarURL({
                     format: 'png',
                     size: 512
                 }))
-                .setThumbnail(user.displayAvatarURL({
+                .setThumbnail(member.user.displayAvatarURL({
                     format: 'png',
                     dynamic: true,
                     size: 1024
                 }))
-                .addField(message.guild.lang.COMMANDS.USERINFO.username, user.tag, true)
-                .addField("Type", user.bot ? message.guild.lang.COMMANDS.USERINFO.bot : message.guild.lang.COMMANDS.USERINFO.user, true)
-                .addField("Badges", user.flags ? user.flags.toArray().filter(b => b !== "VERIFIED_DEVELOPER").map(badge => `${badgesEmotes[badge]}`).join(" ") : message.guild.lang.COMMANDS.USERINFO.none)
-                .addField(message.guild.lang.COMMANDS.USERINFO.currentStatus, user.presence.status.charAt(0).toUpperCase() + user.presence.status.slice(1), true)
-                .addField(message.guild.lang.COMMANDS.USERINFO.accountCreated, `${timeZoneConvert(user.createdAt).split(/ +/).splice(0, 3).join(' ')} | ${message.guild.lang.COMMANDS.USERINFO.accountCreatedAgo(ms(new Date().getTime() - user.createdTimestamp, { long: true }))}`)
+                .addField(message.guild.lang.COMMANDS.USERINFO.username, member.user.tag, true)
+                .addField("Type", member.user.bot ? message.guild.lang.COMMANDS.USERINFO.bot : message.guild.lang.COMMANDS.USERINFO.user, true)
+                .addField("Badges", member.user.flags ? member.user.flags.toArray().filter(b => b !== "VERIFIED_DEVELOPER").map(badge => `${badgesEmotes[badge]}`).join(" ") : message.guild.lang.COMMANDS.USERINFO.none)
+                .addField(message.guild.lang.COMMANDS.USERINFO.currentStatus, member.user.presence.status.charAt(0).toUpperCase() + member.user.presence.status.slice(1), true)
+                .addField(message.guild.lang.COMMANDS.USERINFO.accountCreated, `${timeZoneConvert(member.user.createdAt).split(/ +/).splice(0, 3).join(' ')} | ${message.guild.lang.COMMANDS.USERINFO.accountCreatedAgo(ms(new Date().getTime() - member.user.createdTimestamp, { long: true }))}`)
                 .addField(message.guild.lang.COMMANDS.USERINFO.joined, `${timeZoneConvert(member.joinedAt).split(/ +/).splice(0,3).join(' ')}, ${message.guild.lang.COMMANDS.USERINFO.joinedAgo(ms(new Date() - member.joinedTimestamp, { long: true }))}`)
-                .addField(message.guild.lang.COMMANDS.USERINFO.currentlyActiveOn, Object.keys(user.presence.clientStatus || {}).map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(', ') || message.guild.lang.COMMANDS.USERINFO.offline)
+                .addField(message.guild.lang.COMMANDS.USERINFO.currentlyActiveOn, Object.keys(member.user.presence.clientStatus || {}).map(x => x.charAt(0).toUpperCase() + x.slice(1)).join(', ') || message.guild.lang.COMMANDS.USERINFO.offline)
                 .addField(message.guild.lang.COMMANDS.USERINFO.nitroBoostStatus, `${member && member.premiumSince ? `${timeZoneConvert(member.premiumSince).split(/ +/).splice(0,3).join(' ')}, ${message.guild.lang.COMMANDS.USERINFO.nitroBoostStatusAgo(ms(new Date() - member.premiumSinceTimestamp, { long: true }))}` : message.guild.lang.COMMANDS.USERINFO.noNitroBoostStatus}`)
                 .addField("Roles", `${member.roles.cache.filter(role => role.id !== message.guild.id).map(role => `${role}\u2000`).splice(0, 25).join('•\u2000')} ${member.roles.cache.size > 26 ? message.guild.lang.COMMANDS.USERINFO.moreRoles(member.roles.cache.size - 26) : "\u200b"}`)
 
