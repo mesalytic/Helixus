17db1f5359ba3154d3cc4dd2074b50d23111ee99
chocololat <atamatori56@gmail.com>
2021-07-24 22:07:15 +0200

    ref: revamped mongodb database and rpg idea

--- commands/rpg/register.js
+++ commands/rpg/register.js
@@ -1,6 +1,6 @@
 const Command = require("../../structures/Command");
-const { MessageEmbed, Message } = require('discord.js');
-const abilitiesFile = require('../../assets/json/abilities.json')
+const { Message } = require('discord.js');
+const { rpgPlayer } = require("../../structures/DatabaseCollections");
 
 module.exports = class RegisterCommand extends Command {
     constructor(bot) {
@@ -14,89 +14,14 @@ module.exports = class RegisterCommand extends Command {
      * @param {Message} message
      */
     async run(message) {
-
-        const user = await this.bot.mongoDB.Rpg.findById(message.author.id);
-        if (user) return message.channel.send("❌ **|** Adventurer, you are already registered!")
-
-        const classes = [
-            "Assassin",
-            "Barbarian",
-            "C̴̢̡̮̹̩̰̔̌̌̄̑͊̉̊̕͝͝͝͝͝l̶̹̣̪̦̞̰̼̲̉̾́̀̕͘̕̚͘͜͝e̷̩͖̳͓̤͔̿̂͂̇̀͆r̷̪͇͔̈́͆̔̂́̕͜i̵̛̻̖͆̏̓̉̔͆̓̓͒͜͝͝͝ċ̵̙̮͈͚̖̦̣́̈́̈́̑͒̔͑̀̔̒̉̂͜͜͝͝",
-            "D̴͕̼̞̝̅̑̍̉̄́̾̀̃̄͋̋͒͝r̵̬͗̑̓ư̴̹̝͇̻̥̮͍̠͎̱̝̓̍̔̂͌͆͑͗͌̔̽͝ĩ̴̛͇̤̘͕̂͊͐͋̾̾̑͆d̵̨̗̙̳̒̿̑̄̃̔͂͝͠",
-            "S̷̛̭̙̳̍́̒́͋̄̐̊̈̅͊̚͜͠͠w̴̲̗̯̰̤̹̺͙͍͍̯̹̮͕̒͜o̷̼͎̖͓̲͇͙̥͍̲͆̏͠ͅr̴̨̡̩̞̲̤̟͙̖̜͔̥͓͈̎͌͋̓͐̄̄̄̑̒̂̚d̵̻̘̖͔̯̦̹̮̞̮͚͓̯̔̆̃́͘̕ş̵̥̟̞̬̹̟͚̦̮͇̻̗̂͛͐͛̍͋͠m̸̢̢̡̠͚̦͍͕̼͇̫͇̫̣̏̇̇̌̈̔̚͠͝ͅa̸̛̤̲͇̝͙̹̦͂̂n̴̨̢̻͎͖͖̺̞̻͓̭̰̔͒̽̔̈́̒̑̐͛͊͘͘͝͠",
-            "S̶̰̟̳͇̪̦̖̤̥̻͎͐͆͒̑̂͒̓͑͘ơ̷̧͉̱̖̖̪̈́̿̓̒͂̅̐̌̃̆̆̒͠͝ŗ̶̛̤͓̼̘̉̉̂̽̀̓̓́̔͝c̴̮̙̮̺͚̤̖̭̺̘͋̾̊͛̎̈́̈́̽̉̏͠ḙ̶̓́̋̾͗̾̂̒̎̄̓͐r̵̨̡̟͙͎͎̭͚̮̳̣̜͚̄̿̌͌̅͛̒͗͝e̶̡̙̲̹͇͈̮̤̣̞͛̾̏̌̀̏̕͘r̴̺͎͉͓̙̩̻̈́͒̊̽̊̅̐̄̐̊̓͘",
-            "M̷̦̘̔͆̈́͑̓̎͆̓̉͊ở̴̢͚̼̞̮̞͎͚͖̳̥̘̰̯͂͊͂́̔̉͐́͆͝͝n̷̢̙̺̋͑̎̽̈́̈́͊͘͜͝k̴̡̨̧̞̯̱̩̮̹̪̟̟͋͑̊͋̈́̂̅̀̃͜͝͝͠",
-            "N̷͍̬̱̉͂͋̍̽͝ë̸̢̧̳̱̥͙̞́̍c̷̛̘̗̥͛̐́̆͂́̀̔͂̿̒̅̚̕r̸̡̧̬̤̥̱̜̭̲̜̟̹̱̤̪͑̑̔̀̂̐̀̒̀̚ơ̶͓͒̆̽m̶̡̜̺̥̘̲͖̪͖͕̫͍̓̔̃͊̈́͝a̸̢̬̲͙̬͍͕̗̬̠̗̮̰̦̯͆͗̃̓̾́̂́̈́̊̈ṋ̶̢͙͚͔͓̭̒̆̌̏̈́͐̕͠c̸̛̪͙͍̾̽͒e̶̼̟͈̟͔͖͎̔́ŕ̷̛̺͓͖͆̒̽́̈́̄̾̐"
-        ]
-
-        let description = "Hey there!\nSo, you want to be an adventurer, huh? Great! Great..\nJust, choose the class you want to be already.\n"
-
-        const embed = new MessageEmbed()
-            .setTitle("🍻 | Class Choice")
-            .setColor('#ffec02')
-            .setFooter("Type in the chat option of your choice");
-
-        for (let i = 0; i < classes.length; i++) {
-            description += `\n${i + 1} - **${classes[i]}**`;
-        }
-
-        embed.setDescription(description);
-        message.channel.send(embed);
-
-        const filter = (m) => m.author.id === message.author.id;
-        const collector = message.channel.createMessageCollector(filter, { max: 1 })
-
-        collector.on('collect', (m) => {
-            if (classes[Number(m.content) - 1]) this.confirm(message, classes[Number(m.content) - 1]);
-            else message.reply("❌ **|** Adventurer, it's not a valid class!")
-        })
-    }
-    /**
-     * @param {Message} message
-     * @param {string} option
-     */
-    confirm(message, option) {
-        message.channel.send(`⚠️ **|** ${message.author}, do you really want to be a ${option}? This choice is __**IRREVERSIBLE**__!\n\nType \`yes\` to confirm your choice!`);
-
-        const filter = (m) => m.author.id === message.author.id;
-        const collector = message.channel.createMessageCollector(filter, { max: 1 });
-
-        collector.on('collect', async (m) => {
-            if (m.content.toLowerCase() === 'yes') {
-                message.channel.send(`✅ **|** Well, welcome aboard, ${option}! Do \`am!help\`, and search for the category \`RPG\` to see all the available commands!`)
-                const user = await new this.bot.mongoDB.Rpg({
-                    _id: message.author.id,
-                    class: option,
-                }).save();
-
-                this.confirmRegister(message, user);
-            }
-        })
-    }
-
-    confirmRegister(message, user) {
-        setTimeout(async () => {
-            switch (user.class) {
-                case 'Assassin': {
-                    const unicPowersAssassin = abilitiesFile.assassin.uniquePowers;
-                    const powerRandom = Math.floor(Math.random() * unicPowersAssassin.length);
-                    const choiceAssassin = unicPowersAssassin[powerRandom];
-                    user.armor = 5;
-                    user.damage = 25;
-                    user.mana = 20;
-                    user.maxMana = 20;
-                    user.abilityPower = 1;
-                    user.abilities.push(abilitiesFile.assassin.normalAbilities[0]);
-                    user.weapon = {
-                        name: 'Dagger',
-                        damage: 5,
-                        type: 'Arme',
-                    };
-                    user.uniquePower = choiceAssassin;
-                    await user.save();
-                    message.channel.send(`✅ **|** rpg:registered`)
-                }
-            }
-        }, 1000)
+        message.channel.send(`V - Welcome to the Adventure, ${message.author}!\n\nI recommend you start completing the tutorial quest line, it will help you understand the basics of this adventure!\nYou can start it using \`am!quest\`\n\nI'm pretty sure you'll do amazing, good luck adventurer!`)
+        const account = {
+            username: message.author.username,
+            userId: message.author.id,
+        };
+        const newUser = new this.bot.mongoDB.Rpg({
+            account,
+        });
+        return newUser.save();
     }
 }
--- structures/DatabaseCollections.js
+++ structures/DatabaseCollections.js
@@ -1,36 +1,546 @@
 const mongoose = require('mongoose');
 
 const rpgSchema = mongoose.Schema({
-    _id: { type: String },
-    class: { type: String },
-    life: { type: Number, default: 100 },
-    armor: { type: Number, default: 0 },
-    damage: { type: Number, default: 0 },
-    mana: { type: Number, default: 0 },
-    maxLife: { type: Number, default: 100 },
-    maxMana: { type: Number, default: 20 },
-    abilityPower: { type: Number, default: 0 },
-    level: { type: Number, default: 0 },
-    xp: { type: Number, default: 0 },
-    nextLevelXp: { type: Number, default: 10 },
-    abilities: { type: Array, default: [] },
-    abilitiesCooldown: { type: Array, default: [] },
-    uniquePower: { type: Object },
-    loots: { type: Array, default: [] },
-    inventory: { type: Array, default: [] },
-    money: { type: Number, default: 0 },
-    dungeonCooldown: { type: String, default: '00000000' },
-    death: { type: String, default: '00000000' },
-    weapon: { type: Object },
-    protection: { type: Object, default: { name: 'Standard Armor', armor: 1 } },
-    hotelTime: { type: String, default: '00000000' },
-    inBattle: { type: Boolean, default: false },
-    backpack: { type: Object, default: { name: 'Wolf Skin Backpack' } },
-    resetRoll: { type: Number, default: 0 },
-    jobId: { type: Number, default: 0 },
-    jobCooldown: { type: String, default: '00000000' },
-    familiar: { type: Object, default: {} },
+    account: {
+		username: String,
+		userId: String,
+		bans: {
+			type: Number,
+			default: 0
+		},
+		banTime: Number,
+		testUser:{
+			type: Boolean,
+			default: false,
+		},
+		patreon: {
+			type: String,
+			enum: ["", "Bronze", "Silver", "Gold", "Platinum"],
+			default: "",
+		},
+	},
+	maxPop: {
+		type: Number,
+		default: 10,
+	},
+	maxBuildings: {
+		type: Number,
+		default: 9
+	},
+	cooldowns: {
+        duel: {
+            type: Date,
+            default: 0,
+        },
+        dailyPrize: {
+            type: Date,
+            default: 0,
+        },
+        dungeon: {
+            type: Date,
+            default: 0,
+        },
+        explore: {
+            type: Date,
+            default: 0,
+        },
+        fish: {
+            type: Date,
+            default: 0,
+        },
+        hunt: {
+            type: Date,
+            default: 0,
+        },
+        miniboss:{
+            type:Date,
+            default: 0,
+        },
+        race: {
+            type: Date,
+            default: 0,
+        },
+        raid: {
+            type: Date,
+            default: 0,
+        },
+        tower: {
+            type: Date,
+            default: 0,
+        },
+        weeklyPrize: {
+            type: Date,
+            default: 0,
+        },
+    },
+    resources: {
+        gold: {
+            type: Number,
+            default: 100,
+        },
+        ["oak wood"]: {
+            type: Number,
+            default: 5,
+        },
+        ["yew wood"]: {
+            type: Number,
+            default: 0,
+        },
+        ["barlind wood"]: {
+            type: Number,
+            default: 0,
+        },
+        ["aspen wood"]: {
+            type: Number,
+            default: 0,
+        },
+        ["copper ore"]: {
+            type: Number,
+            default: 10,
+        },
+        ["iron ore"]: {
+            type: Number,
+            default: 0,
+        },
+        ["mithril ore"]: {
+            type: Number,
+            default: 0,
+        },
+        ["burite ore"]: {
+            type: Number,
+            default: 0,
+        },
+        ["bronze bar"]: {
+            type: Number,
+            default: 0,
+        },
+        ["iron bar"]: {
+            type: Number,
+            default: 0,
+        },
+        ["steel bar"]: {
+            type: Number,
+            default: 0,
+        },
+        ["mithril bar"]: {
+            type: Number,
+            default: 0,
+        },
+        ["pyrite bar"]: {
+            type: Number,
+            default: 0,
+        },
+        ["obsidian ore"]: {
+            type: Number,
+            default: 0,
+        },
+    },
+
+	army: {
+		armory: {
+			helmet: {
+				type: Object,
+				default: {},
+			},
+			chest: {
+				type: Object,
+				default: {},
+			},
+			legging: {
+				type: Object,
+				default: {},
+			},
+			weapon: {
+				type: Object,
+				default: {},
+			},
+		},
+		units: {
+			archery: {
+				huntsman: {
+					type: Number,
+					default: 0,
+				},
+				archer: {
+					type: Number,
+					default: 0,
+				},
+				ranger: {
+					type: Number,
+					default: 0,
+				},
+				survivalist: {
+					type: Number,
+					default: 0,
+				},
+				sharpshooter: {
+					type: Number,
+					default: 0,
+				},
+			},
+			barracks: {
+				peasant: {
+					type: Number,
+					default: 5,
+				},
+				militia: {
+					type: Number,
+					default: 0,
+				},
+				guardsman: {
+					type: Number,
+					default: 0,
+				},
+				knight: {
+					type: Number,
+					default: 0,
+				},
+				berserker: {
+					type: Number,
+					default: 0,
+				},
+				justicar: {
+					type: Number,
+					default: 0,
+				},
+			},
+		},
+	},
+
+	world:{
+		currentLocation: {
+			type: String,
+			default: "Grassy Plains",
+		},
+
+		locations: {
+			"Grassy Plains": {
+				available: {
+					type: Boolean,
+					default: true,
+				},
+				explored: [String],
+			},
+			"Misty Mountains": {
+				available: {
+					type: Boolean,
+					default: false,
+				},
+				explored: [String],
+			},
+			"Deep Caves": {
+				available: {
+					type: Boolean,
+					default: false,
+				},
+				explored: [String],
+			},
+		},
+	},
+
+	empire: {
+		type: Array,
+		default: [],
+	},
+	hero: {
+		elo:{
+			type: Number,
+			default: 1200,
+		},
+		health: {
+			type: Number,
+			default: 100,
+		},
+		currentHealth: {
+			type: Number,
+			default: 100,
+		},
+		attack: {
+			type: Number,
+			default: 5,
+		},
+		defense: {
+			type: Number,
+			default: 3,
+		},
+		inventory: {
+            ["Small Healing Potion"]: {
+                type: Number,
+                default: 5,
+            },
+            ["Large Healing Potion"]: {
+                type: Number,
+                default: 0,
+            },
+            ["Enourmous Healing Potion"]: {
+                type: Number,
+                default: 0,
+            },
+            ["Quality Healing Potion"]: {
+                type: Number,
+                default: 0,
+            },
+            ["Mega Healing Potion"]: {
+                type: Number,
+                default: 0,
+            },
+            ["Ultra Healing Potion"]: {
+                type: Number,
+                default: 0,
+            },
+            ["Small Healing Salve"]: {
+                type: Number,
+                default: 0,
+            },
+            ["Large Healing Salve"]: {
+                type: Number,
+                default: 0,
+            },
+        },
+		dungeonKeys:{
+			"CM Key":{
+				type: Number,
+				default: 0,
+			},
+			"Eridian Vase":{
+				type: Number,
+				default: 0
+			},
+			"The One Shell":{
+				type: Number,
+				default: 0
+			}
+		},
+		currentExp: {
+			type: Number,
+			default: 1,
+		},
+		expToNextRank: {
+			type: Number,
+			default: 100,
+		},
+		rank: {
+			type: Number,
+			default: 0,
+		},
+		armor: {
+			helmet: {
+				type: String,
+				default: "[NONE]",
+			},
+			chest: {
+				type: String,
+				default: "[NONE]",
+			},
+			legging: {
+				type: String,
+				default: "[NONE]",
+			},
+			weapon: {
+				type: String,
+				default: "[NONE]",
+			},
+		},
+	},
+	consecutivePrizes:{
+		dailyPrize: {
+			type: Number,
+			default: 0,
+		},
+		weeklyPrize:{
+			type: Number,
+			default: 0,
+		},
+	},
+	// Array of Objects.
+	// quests: [{started: Bolean, questKeySequence: Array, name: String}]
+	quests: {
+		type: [
+			{
+				type: Object,
+			},
+		],
+		default: [{
+			started: false,
+			questKeySequence: ["gettingStarted", "buildMine"],
+			name: "Build a Mine",
+			// pve: [{ // Raid is optional
+			// 	name: String, // e.g: "Collapsed Mine"
+			//	completed: Bolean,
+			// chance: Number, // e.g. 0.5 chance to get it (50%)
+			// },]
+		}],
+	},
+	completedQuests: [String],
+	// Saving the rooms etc, of the towers
+	tower: {
+		"solo full-army": {
+			level: {
+				type: Number,
+				default: 1,
+			}
+		},
+		"trio full-army": {
+			level: {
+				type: Number,
+				default: 1,
+			},
+			users: {
+				type: Array,
+				default: [],
+			}
+		},
+		"solo hero": {
+			level: {
+				type: Number,
+				default: 1,
+			},
+		},
+		"trio hero": {
+			level: {
+				type: Number,
+				default: 1,
+			},
+			users: {
+				type: Array,
+				default: [],
+			}
+		}
+	},
+
+	// object too big, moved to ./uservalues/default
+	statistics: {
+        army: {
+            type: Number,
+            default: 0,
+        },
+        build: {
+            type: Number,
+            default: 0,
+        },
+        buy: {
+            type: Number,
+            default: 0,
+        },
+        collect: {
+            type: Number,
+            default: 0,
+        },
+        cooldown: {
+            type: Number,
+            default: 0,
+        },
+        craft: {
+            type: Number,
+            default: 0,
+        },
+        dailyprize: {
+            type: Number,
+            default: 0,
+        },
+        duel: {
+            type: Number,
+            default: 0,
+        },
+        dungeon: {
+            type: Number,
+            default: 0,
+        },
+        equip: {
+            type: Number,
+            default: 0,
+        },
+        explore: {
+            type: Number,
+            default: 0,
+        },
+        fish: {
+            type: Number,
+            default: 0,
+        },
+        grid: {
+            type: Number,
+            default: 0,
+        },
+        help: {
+            type: Number,
+            default: 0,
+        },
+        hunt: {
+            type: Number,
+            default: 0,
+        },
+        info: {
+            type: Number,
+            default: 0,
+        },
+        look: {
+            type: Number,
+            default: 0,
+        },
+        miniboss: {
+            type: Number,
+            default: 0,
+        },
+        produce: {
+            type: Number,
+            default: 0,
+        },
+        profile: {
+            type: Number,
+            default: 0,
+        },
+        quest: {
+            type: Number,
+            default: 0,
+        },
+        race: {
+            type: Number,
+            default: 0,
+        },
+        raid: {
+            type: Number,
+            default: 0,
+        },
+        rank: {
+            type: Number,
+            default: 0,
+        },
+        recruit: {
+            type: Number,
+            default: 0,
+        },
+        resources: {
+            type: Number,
+            default: 0,
+        },
+        stake: {
+            type: Number,
+            default: 0,
+        },
+        tower: {
+            type: Number,
+            default: 0,
+        },
+        travel: {
+            type: Number,
+            default: 0,
+        },
+        use: {
+            type: Number,
+            default: 0,
+        },
+        weeklyprize: {
+            type: Number,
+            default: 0,
+        },
+    }
+}, {
+	timestamps: {
+		createdAt: "createdAt",
+		updatedAt: "updatedAt",
+	},
 });
 
-const rpg = mongoose.model('rpg', rpgSchema);
+const rpg = mongoose.model('rpgPlayer', rpgSchema);
 module.exports.Rpg = rpg;
