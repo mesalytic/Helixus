61c717b6d25ba060baa91856c003c318e81f5969
chocololat <atamatori56@gmail.com>
2021-07-30 16:10:30 +0200

    fix(Register): check if already registered

--- commands/rpg/register.js
+++ commands/rpg/register.js
@@ -14,6 +14,8 @@ module.exports = class RegisterCommand extends Command {
      * @param {Message} message
      */
     async run(message) {
+        let dbUser = await this.bot.mongoDB.Rpg.findOne({ "account.userId": message.author.id })
+        if (dbUser) return message.reply("X - You're already registered, adventurer.")
         message.channel.send(`V - Welcome to the Adventure, ${message.author}!\n\nI recommend you start completing the tutorial quest line, it will help you understand the basics of this adventure!\nYou can start it using \`am!quest\`\n\nI'm pretty sure you'll do amazing, good luck adventurer!`)
         const account = {
             username: message.author.username,
